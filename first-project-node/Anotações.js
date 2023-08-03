1321ffe92445471e3ccc87240ffe6d2f096511bd
JohnatanChagas <johnatan_pereira@hotmail.com>
2023-08-03 13:39:16 -0300

    finished my first project node

---
+++ first-project-node/Anotações.js
@@ -0,0 +1,38 @@
+/*
+ Node -> Ambiente de execução javascripit
+ - Não é linguagem
+ - Não é biblioteca
+
+    VANTAGENS Sobre outras (Diferenças)
+ - Mesma liguagem - front e back
+ - Usado por grandes empresas( Netflix, Facebook)
+ - Performace
+
+    V8 - Motor de interpretação Javascript
+    Ele compila o js para formato nativo de máquina
+
+    Request - requisição - web
+    response - resposta - servidor
+
+*/
+    
+//* Verbos HTTP
+//! -GET -leitura - só funciona rota pelo navegador
+//! -POST - criação
+//! -PUT - atualização
+//! -DELETE - deleção
+//! -PATCH - Atualização parcial
+
+   //* Paramentros
+//! -200 -> confirmação/sucesso
+//! -400 -> erro do cliente (front)
+//! -500 -> erro servidor (falha ao concluir solicitação)
+
+//? Iniciar projeto node pelo terminal vscode
+//! npm init -y - cria package.json
+//! npm i express - instala o freme
+//? Após deletar pasta node_modules para subir para o repositótio, quando abrir o projeto é so usar (i express)
+
+//! npm i nodemon -D -> para não ficar rodando e parando o servido toda vez que modifica o codigo. npm run dev
+
+
